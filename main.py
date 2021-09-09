a5bc41b17b254a58c000894ce95fc6a07ec44944
GerardoMunozUD <80701273+GerardoMunoz-UD@users.noreply.github.com>
2021-09-09 20:59:45 +0000

    Se adicionan las válvulas

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 import tkinter as tk
-#from signal import pause
-#from PIL import ImageTk, Image
-import time
+from math import sqrt
+import time, threading
+tiempo_global = time.time()
+
 
 window = tk.Tk()
 window.title("Hello wold")
-window.geometry("300x300")
-
-hello = tk.Label(text="Hello world!")
-hello.pack()
-
 
 def saluda_porllamadoatrasado():
     print('Saludando')
 
 
-button_adicionar = tk.Button(
-    text="Adicionar_T1")  #, command=saluda_porllamadoatrasado)
-button_adicionar.pack()
-button_sustraer = tk.Button(text="Sustraer_T1",
-                            command=saluda_porllamadoatrasado)
-button_sustraer.pack()
-#button.configure(text = "Goodbye World!", command=goodbye_world)
-button_adicionar.configure(command=saluda_porllamadoatrasado)
+button_valvula1 = tk.Button( text="Valvula1")
+button_valvula1.pack(side=tk.TOP)
+button_valvula2 = tk.Button(text="Valvula2")
+button_valvula2.pack(side=tk.TOP)
+button_valvula3 = tk.Button(text="Valvula3")
+button_valvula3.pack(side=tk.TOP)
+label_vacio = tk.Label(text="Vacío")
+label_vacio.pack(side=tk.TOP)
+label_nivel1 = tk.Label(text="Nivel 1")
+label_nivel1.pack(side=tk.TOP)
+label_nivel2 = tk.Label(text="Nivel 2")
+label_nivel2.pack(side=tk.TOP)
 
 canvas = tk.Canvas(window, width=800, height=800)
-canvas.pack()
+canvas.pack(side=tk.BOTTOM)
 
 img = tk.PhotoImage(file="planta.gif")
 canvas.create_image(0, 0, anchor=tk.NW, image=img)
@@ -34,8 +34,9 @@ canvas.create_image(0, 0, anchor=tk.NW, image=img)
 class Tanque:
 
   
-    def __init__(self, capacidad_total, capacidad_inicial, izquierda, abajo,
+    def __init__(self, id,capacidad_total, capacidad_inicial, izquierda, abajo,
                  derecha, arriba):
+        self.id=id
         self.capacidad_total = capacidad_total
         self.capacidad_actual = capacidad_inicial
         self.izquierda = izquierda
@@ -44,29 +45,18 @@ class Tanque:
         self.arriba = arriba
 
         alto = abajo - arriba
-        ancho = derecha - izquierda
         altura_liquido = int(alto * self.capacidad_actual /
                              self.capacidad_total)
 
         self.aire = canvas.create_rectangle(
-            izquierda,
-            abajo - alto,
-            #izquierda+ancho, abajo-alto,
-            izquierda + ancho,
-            abajo - altura_liquido,
-            #izquierda, abajo-altura_liquido,
+            izquierda, arriba,
+            derecha, abajo - altura_liquido,
             fill='white',
-            #outline='green',
-        )
+       )
         self.liquido = canvas.create_rectangle(
-            izquierda,
-            abajo - altura_liquido,
-            #izquierda+ancho, abajo-altura_liquido,
-            izquierda + ancho,
-            abajo,
-            #izquierda, abajo,
+            izquierda, abajo - altura_liquido,
+            derecha, abajo,
             fill='blue',
-            #outline='green',
         )
 
     def dibujo_tkinter(self):
@@ -77,26 +67,18 @@ class Tanque:
         derecha = self.derecha
         arriba = self.arriba
         alto = abajo - arriba
-        ancho = derecha - izquierda
         altura_liquido = int(alto * capacidad_actual / capacidad_total)
-        #Dibuja un cuadrado
+        # Dibuja un rectángulo para representar la parte ocupada 
+        # y otro para el resto
         canvas.coords(
             self.aire,
-            izquierda,
-            abajo - alto,
-            #izquierda+ancho, abajo-alto,
-            izquierda + ancho,
-            abajo - altura_liquido,
-            #izquierda, abajo-altura_liquido,
+            izquierda, arriba,
+            derecha, abajo - altura_liquido,
         )
         canvas.coords(
             self.liquido,
-            izquierda,
-            abajo - altura_liquido,
-            #izquierda+ancho, abajo-altura_liquido,
-            izquierda + ancho,
-            abajo,
-            #izquierda, abajo,
+            izquierda, abajo - altura_liquido,
+            derecha, abajo,
         )
 
         return self
@@ -105,61 +87,115 @@ class Tanque:
         self.capacidad_actual += cantidad
         self.dibujo_tkinter()
 
+    def deja_salir(self,t):
+        S1=10 # area arriba
+        S2=0.3  # area de la valvula
+        #g=10
+        H=self.capacidad_actual/S1
+
+        # según http://www.sc.ehu.es/sbweb/fisica3/fluidos/vaciado/vaciado.html
+        #h=(sqrt(H)-S2*sqrt(2*9.8/(S1^2-S2^2))*t/2).^2;
+        if H>=0:
+            h=(sqrt(H)-S2*sqrt(2*9.8/(S1**2-S2**2))*t/2)**2
+        else:
+            h=0
+        volumen=(H-h)*S1
+        print('deja_salir',t,H,sqrt(H),S2*sqrt(2*9.8/(S1**2-S2**2))*t/2)#H,h,S1,volumen
+        self.sustraer(volumen)
+        return volumen
+
     def sustraer(self, cantidad):
         self.adicionar(-cantidad)
 
-    def sustraer10(self):
-        self.sustraer(10)
+class Caldera(Tanque):
+    def __init__(self, id,capacidad_total, capacidad_inicial, izquierda, abajo,
+                 derecha, arriba, valor_vacio, valor_nivel_1, valor_nivel_2 ):
+        super().__init__(id,capacidad_total, capacidad_inicial, izquierda, abajo,
+                 derecha, arriba)
+        self.valor_vacio   = valor_vacio
+        self.valor_nivel_1 = valor_nivel_1
+        self.valor_nivel_2 = valor_nivel_2
 
-    def adicionar10(self):
-        self.adicionar(10)
+    def esta_vacio(self):
+        return (self.capacidad_actual <= self.valor_vacio)
+   
+    def esta_nivel_1(self):
+        return (self.capacidad_actual >= self.valor_nivel_1)
+   
+    def esta_nivel_2(self):
+        return (self.capacidad_actual >= self.valor_nivel_2)
+   
+
+class Valvula:
+
+    def color_valvula(self):
+        return 'red' if self.valvula_abierta else 'green'
+
+  
+    def __init__(self,id, flujo_por_segundo, valvula_abierta, izquierda, abajo,
+                 derecha, arriba, tanque_sustrae, tanque_adiciona):
+        self.id=id
+        self.valvula_abierta = valvula_abierta
+        self.flujo_por_segundo = flujo_por_segundo
+        self.izquierda = izquierda
+        self.abajo = abajo
+        self.derecha = derecha
+        self.arriba = arriba
+        self.tanque_sustrae = tanque_sustrae
+        self.tanque_adiciona = tanque_adiciona
 
 
-t1 = Tanque(1000, 800, 65, 142, 184, 37)
-t2 = Tanque(1000, 800, 288, 146, 406, 40)
-c = Tanque(100, 80, 155, 295, 325, 196)
-t3 = Tanque(1000, 800, 295, 452, 488, 353)
-button_adicionar.configure(command=c.adicionar10)
-button_sustraer.configure(command=c.sustraer10)
+        self.dibujo = canvas.create_polygon(
+            izquierda, arriba,
+            izquierda, abajo,
+            derecha, arriba,
+            derecha, abajo,
+            fill=self.color_valvula(),
+        )
 
-tk.mainloop(
-)  # El mainloop permite la ejecución de cada callback (llamadoatrasado)
-#------------------------------------Ver1
 
-# import tkinter as tk
-# #from PIL import ImageTk, Image
+    def actualiza_valores(self,tiempo):
+        if self.valvula_abierta:
+            self.tanque_adiciona.adicionar(self.tanque_sustrae.deja_salir(tiempo))
 
-# # a continuacion se define el callback (llamadoatrasado) para el boton
-# def saluda_porllamadoatrasado():
-#   print('Hola')
+    def cambia_estado(self):
+        #print('cambia_estado',self.valvula_abierta)
+        self.valvula_abierta = not(self.valvula_abierta)
+        #print('cambia_estado',self.valvula_abierta)
+        canvas.itemconfig(self.dibujo,fill=self.color_valvula())
 
-# window = tk.Tk()
-# window.title("Hello wold")
-# window.geometry("300x300")
 
-# hello = tk.Label(text="Hello world!")
-# hello.pack()
-# button = tk.Button(text="Click me!", command=saluda_porllamadoatrasado)
-# button.pack()
+t1 = Tanque(1,400, 300, 65, 142, 184, 37)
+t2 = Tanque(2,400, 300, 288, 146, 406, 40)
+c  = Caldera(3,100, 0, 155, 295, 325, 196,10,50,80)
+t3 = Tanque(4,1000, 0, 295, 452, 488, 353)
+v1 = Valvula(5,5,False,143,170,181,160,t1,c)
+v2 = Valvula(6,5,False,286,176,325,162,t2,c)
+v3 = Valvula(7,5,False,291,330,330,315,c,t3)
+button_valvula1.configure(command=v1.cambia_estado)
+button_valvula2.configure(command=v2.cambia_estado)
+button_valvula3.configure(command=v3.cambia_estado)
+#canvas.itemconfig(label_vacio,fill=self.color_valvula())
 
-# canvas=tk.Canvas(window, width=800,height=800)
-# canvas.pack()
-# img = tk.PhotoImage(file="planta.gif")
-# canvas.create_image(0,0,anchor=tk.NW,image=img)
 
-# #Dibuja un cuadrado
-# canvas.create_polygon(10, 10,
-#                       100, 10,
-#                       100, 100,
-#                       10, 100,
-#                       fill='red',
-#                       outline='green')
+finaliza = False
+def actualizacion_periodica():
 
-# #dibuja un triangulo
-# canvas.create_polygon(10, 150,
-#                       100, 200,
-#                       10, 250,
-#                       fill='blue',
-#                       outline='yellow')
 
-# tk.mainloop()
+    if not finaliza:
+        threading.Timer(1, actualizacion_periodica).start()
+        global tiempo_global
+        #print(time.ctime())
+        tiempo_actual = time.time()
+        tiempo = tiempo_actual - tiempo_global
+        tiempo_global = tiempo_actual
+        v1.actualiza_valores(tiempo)
+        v2.actualiza_valores(tiempo)
+        v3.actualiza_valores(tiempo)
+    else:
+        print('Finalizó')
+
+actualizacion_periodica()
+tk.mainloop()  
+finaliza=True
+print('Finalizando')
